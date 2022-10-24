fbc044d774381be5ab812d6c9c3a20d3fb4f1232
손준혁 <sonjuhy@gmail.com>
2022-10-24 09:50:47 +0900

    ✨ push sample spring boot
    - push sample user spring boot
    
    ✨ : 새로운 기능 추가

---
+++ backend/user/src/test/java/com/ssafy/e205/E205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.e205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E205ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
