c909ba8652003bba0a86b56cedca7d1cea569b56
leedayeong <98dlstod@gmail.com>
2022-10-25 15:32:56 +0900

    🔧 연도별, 행정동별, 업종별, 매출 데이터 전처리
    - 연도별, 행정동별, 업종별, 매출 데이터 전처리
    - 이전은 연도별, 상권_코드 별, 업종별, 매출 데이터 였음 ^^ ㅈㅅ
    
    
    Former-commit-id: 8a15aa70fd0fb911645b22fe40367761cd54e0c1

---
+++ backend/parse_python/groupby_sales__dong_name.py
@@ -0,0 +1,21 @@
+#라이브러리 import
+# 연도별 행정동별 업종별 매출데이터
+import requests
+import pprint
+import json
+import pandas as pd
+import numpy as np
+
+df_sales = pd.read_excel(r'C:\Users\SSAFY\Desktop\E205\S07P31E205\backend\parse_python\year_dong\year_dong_2021.xlsx')
+df_sales
+df_sales.drop('Unnamed: 0', axis=1, inplace=True)
+len(df_sales)
+# len(np.unique(df_sales[['상권_코드', '서비스_업종_코드']], axis=0))
+
+print(df_sales.info())
+df_sales = df_sales.astype({'기준_년_코드':'object', '상권_코드':'object', '기준_분기_코드':'object'})
+df = df_sales.groupby(['기준_년_코드', '시군구', '행정동', '서비스_업종_코드', '서비스_업종_코드_명'], as_index=False).sum()
+df
+df.rename(columns = {'분기당_매출_금액':'매출_금액'},inplace=True)
+df.to_excel("sales_year_dong_2021.xlsx")
+
