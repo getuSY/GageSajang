969bb7da2a86e0d6110f43a3a4abc2f0663c93e7
손준혁 <sonjuhy@gmail.com>
2022-11-01 02:37:32 +0900

    ################ # <이모지> <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 ✨ ADD DB concurrency prevention code
    # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - ADD DB concurrency prevention code
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # Jira Smart commit 사용 시 Jira 이슈 또한 종료할 수 있음.
    # 예) Close #7
    
    ################
    # ✨ : 새로운 기능 추가
    # 🐛 : 버그 수정
    # 📚 : 문서 수정
    # 🚨 : 테스트 코드 추가
    # 🔨 : 코드 리팩토링
    # 📝 : 코드 의미에 영향을 주지 않는 변경사항
    # 🔧 : 기타 변경사항, 프로덕션 코드 변경사항 없음
    ################

--- backend/user/src/main/java/com/ssafy/e205/db/repository/SalesDayRepository.java
+++ backend/user/src/main/java/com/ssafy/e205/db/repository/SalesDayRepository.java
@@ -3,10 +3,17 @@ package com.ssafy.e205.db.repository;
 import com.ssafy.e205.api.dto.SalesDayDto;
 import com.ssafy.e205.db.entity.SalesDayEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.List;
 
 public interface SalesDayRepository extends JpaRepository<SalesDayEntity, Integer> {
+    @Lock(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
+    @QueryHints({@QueryHint(name="javax.persistence.lock.timeout", value = "10000")}) //mariaDB 에선 작동안함
+
     List<SalesDayEntity> findAll();
     List<SalesDayEntity> findByEmail(String email);
     SalesDayEntity findByEmailAndYearAndMonthAndDay(String email, int year, int month, int day);
--- backend/user/src/main/java/com/ssafy/e205/db/repository/SalesMonthRepository.java
+++ backend/user/src/main/java/com/ssafy/e205/db/repository/SalesMonthRepository.java
@@ -5,10 +5,17 @@ import com.ssafy.e205.api.dto.SalesMonthDto;
 import com.ssafy.e205.db.entity.SalesDayEntity;
 import com.ssafy.e205.db.entity.SalesMonthEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.List;
 
 public interface SalesMonthRepository extends JpaRepository<SalesMonthEntity, Integer> {
+    @Lock(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
+    @QueryHints({@QueryHint(name="javax.persistence.lock.timeout", value = "10000")}) //mariaDB 에선 작동안함
+
     List<SalesMonthEntity> findAll();
     List<SalesMonthEntity> findByEmail(String email);
     SalesMonthEntity findByEmailAndYearAndMonth(String email, int year, int month);
