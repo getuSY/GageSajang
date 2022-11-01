b7cf9e032ce6bfcc3e8711a1644d9d58e804635e
leedayeong <98dlstod@gmail.com>
2022-11-01 14:22:28 +0900

    🔧 더미 데이터 추가 ( 2014, 2015, 2021, 2022 더미 데이터 생성)
    - 🔧 더미 데이터 추가 ( 2014, 2015, 2021, 2022 더미 데이터 생성)
    
    
    Former-commit-id: 97435651f221b5735931a74dccce8ee5a8d8214b

---
+++ backend/parse_python/code/simul_dummy_data.py
@@ -0,0 +1,42 @@
+#라이브러리 import
+# 연도별 행정동별 업종별 매출데이터
+
+import pandas as pd
+import numpy as np
+
+df_sales = pd.read_excel(r'C:\Users\SSAFY\Desktop\E205\tmp\S07P31E205\backend\parse_python\real_final_2021.xlsx')
+df_sales.info()
+
+df_dummy = df_sales.copy()
+df_dummy.info()
+
+year = 2022
+ratio = 1.3
+
+df_dummy['기준_년_코드'] = year
+
+df_dummy['분기당_매출_건수'] = df_sales['분기당_매출_건수'] * ratio
+df_dummy['점포수'] = df_sales['점포수'] * ratio
+df_dummy['유사_업종_점포_수'] = df_sales['유사_업종_점포_수'] * ratio
+df_dummy['개업_점포_수'] = df_sales['개업_점포_수'] * ratio
+df_dummy['폐업_점포_수'] = df_sales['폐업_점포_수'] * ratio
+df_dummy['프랜차이즈_점포_수'] = df_sales['프랜차이즈_점포_수'] * ratio
+df_dummy['총_생활인구_수'] = df_sales['총_생활인구_수'] * ratio
+df_dummy['남성_생활인구_수'] = df_sales['남성_생활인구_수'] * ratio
+df_dummy['여성_생활인구_수'] = df_sales['여성_생활인구_수'] * ratio
+df_dummy['월_평균_소득_금액'] = df_sales['월_평균_소득_금액'] * ratio
+df_dummy['소득_구간_코드'] = df_sales['소득_구간_코드'] *ratio
+df_dummy['지출_총금액'] = df_sales['지출_총금액'] * ratio
+df_dummy['총 상주인구 수'] = df_sales['총 상주인구 수'] * ratio
+df_dummy['총_직장_인구_수'] = df_sales['총_직장_인구_수'] * ratio
+df_dummy['남성_직장_인구_수'] = df_sales['남성_직장_인구_수'] * ratio
+df_dummy['여성_직장_인구_수'] = df_sales['여성_직장_인구_수'] * ratio
+df_dummy['아파트_평균_시가'] = df_sales['아파트_평균_시가'] * ratio
+df_dummy['집객시설_수'] = df_sales['집객시설_수'] *ratio
+df_dummy['분기당_매출_금액/점포수'] = df_sales['분기당_매출_금액/점포수'] *ratio
+
+df = df_dummy.astype({'분기당_매출_건수':'int64', '점포수':'int64', '유사_업종_점포_수':'int64', '개업_점포_수':'int64', '폐업_점포_수':'int64', '프랜차이즈_점포_수':'int64', '총_생활인구_수':'int64',  '남성_생활인구_수':'int64',  '여성_생활인구_수':'int64', '월_평균_소득_금액':'int64', '소득_구간_코드':'int64', '지출_총금액':'int64', '총 상주인구 수':'int64', '총_직장_인구_수':'int64', '남성_직장_인구_수':'int64', '여성_직장_인구_수':'int64', '아파트_평균_시가':'int64', '집객시설_수':'int64', '분기당_매출_금액/점포수':'int64'})
+
+
+df.to_excel("real_final_2022.xlsx", index=False)
+
--- backend/parse_python/code/simul_groupby_total__dong_name.py
+++ backend/parse_python/code/simul_groupby_total__dong_name.py
@@ -4,7 +4,7 @@
 import pandas as pd
 import numpy as np
 
-df_sales = pd.read_excel(r'C:\Users\SSAFY\Desktop\E205\data\final\final_with_dong\simul_final_with_dong_2018.xlsx')
+df_sales = pd.read_excel(r'C:\Users\SSAFY\Desktop\E205\data\final\final_with_dong\simul_final_with_dong_2021.xlsx')
 df_sales
 df_sales.drop('Unnamed: 0', axis=1, inplace=True)
 
@@ -17,55 +17,90 @@ df
 # df.rename(columns = {'분기당_매출_금액':'매출_금액'},inplace=True)
 df.info()
 df = df.astype({'기준_년_코드':'object', '기준_분기_코드':'object'})
-df['분기당_매출_금액'] = df['분기당_매출_금액'].fillna(0)
-df['분기당_매출_금액'] = df['분기당_매출_금액'].replace(np.inf, 0)
-df['분기당_매출_건수'] = df['분기당_매출_건수'].fillna(0)
-df['분기당_매출_건수'] = df['분기당_매출_건수'].replace(np.inf, 0)
-df['점포수'] = df['점포수'].fillna(0)
-df['점포수'] = df['점포수'].replace(np.inf, 0)
-df['유사_업종_점포_수'] = df['유사_업종_점포_수'].fillna(0)
-df['유사_업종_점포_수'] = df['유사_업종_점포_수'].replace(np.inf, 0)
-df['개업_점포_수'] = df['개업_점포_수'].fillna(0)
-df['개업_점포_수'] = df['개업_점포_수'].replace(np.inf, 0)
+df['분기당_매출_금액'] = df['분기당_매출_금액'].fillna(df['분기당_매출_금액'].mean())
+df['분기당_매출_금액'] = df['분기당_매출_금액'].replace(np.inf, df['분기당_매출_금액'].mean())
+df['분기당_매출_건수'] = df['분기당_매출_건수'].replace(0, df['분기당_매출_건수'].mean())
 
-df['폐업_점포_수'] = df['폐업_점포_수'].fillna(0)
-df['폐업_점포_수'] = df['폐업_점포_수'].replace(np.inf, 0)
-df['프랜차이즈_점포_수'] = df['프랜차이즈_점포_수'].fillna(0)
-df['프랜차이즈_점포_수'] = df['프랜차이즈_점포_수'].replace(np.inf, 0)
+df['분기당_매출_건수'] = df['분기당_매출_건수'].fillna(df['분기당_매출_건수'].mean())
+df['분기당_매출_건수'] = df['분기당_매출_건수'].replace(np.inf, df['분기당_매출_건수'].mean())
+df['분기당_매출_건수'] = df['분기당_매출_건수'].replace(0, df['분기당_매출_건수'].mean())
+
+df['점포수'] = df['점포수'].fillna(df['점포수'].mean())
+df['점포수'] = df['점포수'].replace(np.inf, df['점포수'].mean())
+df['점포수'] = df['점포수'].replace(0, df['점포수'].mean())
+
+df['유사_업종_점포_수'] = df['유사_업종_점포_수'].fillna(df['유사_업종_점포_수'].mean())
+df['유사_업종_점포_수'] = df['유사_업종_점포_수'].replace(np.inf, df['유사_업종_점포_수'].mean())
+df['유사_업종_점포_수'] = df['유사_업종_점포_수'].replace(0, df['유사_업종_점포_수'].mean())
+
+df['개업_점포_수'] = df['개업_점포_수'].fillna(df['개업_점포_수'].mean())
+df['개업_점포_수'] = df['개업_점포_수'].replace(np.inf, df['개업_점포_수'].mean())
+df['개업_점포_수'] = df['개업_점포_수'].replace(0, df['개업_점포_수'].mean())
+
+df['폐업_점포_수'] = df['폐업_점포_수'].fillna(df['폐업_점포_수'].mean())
+df['폐업_점포_수'] = df['폐업_점포_수'].replace(np.inf, df['폐업_점포_수'].mean())
+df['폐업_점포_수'] = df['폐업_점포_수'].replace(0, df['폐업_점포_수'].mean())
+
+df['프랜차이즈_점포_수'] = df['프랜차이즈_점포_수'].fillna(df['프랜차이즈_점포_수'].mean())
+df['프랜차이즈_점포_수'] = df['프랜차이즈_점포_수'].replace(np.inf, df['프랜차이즈_점포_수'].mean())
+df['프랜차이즈_점포_수'] = df['프랜차이즈_점포_수'].replace(0, df['프랜차이즈_점포_수'].mean())
+
 df['총_생활인구_수'] = df['총_생활인구_수'].fillna(df['총_생활인구_수'].mean())
 df['총_생활인구_수'] = df['총_생활인구_수'].replace(np.inf, df['총_생활인구_수'].mean())
+df['총_생활인구_수'] = df['총_생활인구_수'].replace(0, df['총_생활인구_수'].mean())
+
 df['남성_생활인구_수'] = df['남성_생활인구_수'].fillna(df['남성_생활인구_수'].mean())
 df['남성_생활인구_수'] = df['남성_생활인구_수'].replace(np.inf, df['남성_생활인구_수'].mean())
+df['남성_생활인구_수'] = df['남성_생활인구_수'].replace(0, df['남성_생활인구_수'].mean())
+
 df['여성_생활인구_수'] = df['여성_생활인구_수'].fillna(df['여성_생활인구_수'].mean())
 df['여성_생활인구_수'] = df['여성_생활인구_수'].replace(np.inf, df['여성_생활인구_수'].mean())
+df['여성_생활인구_수'] = df['여성_생활인구_수'].replace(0, df['여성_생활인구_수'].mean())
 
 df['월_평균_소득_금액'] = df['월_평균_소득_금액'].fillna(df['월_평균_소득_금액'].mean())
 df['월_평균_소득_금액'] = df['월_평균_소득_금액'].replace(np.inf, df['월_평균_소득_금액'].mean())
+df['월_평균_소득_금액'] = df['월_평균_소득_금액'].replace(0, df['월_평균_소득_금액'].mean())
+
 df['소득_구간_코드'] = df['소득_구간_코드'].fillna(df['소득_구간_코드'].mean())
 df['소득_구간_코드'] = df['소득_구간_코드'].replace(np.inf, df['소득_구간_코드'].mean())
+df['소득_구간_코드'] = df['소득_구간_코드'].replace(0, df['소득_구간_코드'].mean())
+
 df['지출_총금액'] = df['지출_총금액'].fillna(df['지출_총금액'].mean())
 df['지출_총금액'] = df['지출_총금액'].replace(np.inf, df['지출_총금액'].mean())
+df['지출_총금액'] = df['지출_총금액'].replace(0, df['지출_총금액'].mean())
+
 df['총 상주인구 수'] = df['총 상주인구 수'].fillna(df['총 상주인구 수'].mean())
 df['총 상주인구 수'] = df['총 상주인구 수'].replace(np.inf, df['총 상주인구 수'].mean())
+df['총 상주인구 수'] = df['총 상주인구 수'].replace(0, df['총 상주인구 수'].mean())
+
 df['총_직장_인구_수'] = df['총_직장_인구_수'].fillna(df['총_직장_인구_수'].mean())
 df['총_직장_인구_수'] = df['총_직장_인구_수'].replace(np.inf, df['총_직장_인구_수'].mean())
-df['총_직장_인구_수'] = df['총_직장_인구_수'].fillna(df['총_직장_인구_수'].mean())
-df['총_직장_인구_수'] = df['총_직장_인구_수'].replace(np.inf, df['총_직장_인구_수'].mean())
+df['총_직장_인구_수'] = df['총_직장_인구_수'].replace(0, df['총_직장_인구_수'].mean())
 
 df['남성_직장_인구_수'] = df['남성_직장_인구_수'].fillna(df['남성_직장_인구_수'].mean())
 df['남성_직장_인구_수'] = df['남성_직장_인구_수'].replace(np.inf, df['남성_직장_인구_수'].mean())
+df['남성_직장_인구_수'] = df['남성_직장_인구_수'].replace(0, df['남성_직장_인구_수'].mean())
+
 df['여성_직장_인구_수'] = df['여성_직장_인구_수'].fillna(df['여성_직장_인구_수'].mean())
 df['여성_직장_인구_수'] = df['여성_직장_인구_수'].replace(np.inf, df['여성_직장_인구_수'].mean())
+df['여성_직장_인구_수'] = df['여성_직장_인구_수'].replace(0, df['여성_직장_인구_수'].mean())
+
 df['아파트_평균_시가'] = df['아파트_평균_시가'].fillna(df['아파트_평균_시가'].mean())
 df['아파트_평균_시가'] = df['아파트_평균_시가'].replace(np.inf, df['아파트_평균_시가'].mean())
+df['아파트_평균_시가'] = df['아파트_평균_시가'].replace(0, df['아파트_평균_시가'].mean())
+
 df['집객시설_수'] = df['집객시설_수'].fillna(df['집객시설_수'].mean())
 df['집객시설_수'] = df['집객시설_수'].replace(np.inf, df['집객시설_수'].mean())
+df['집객시설_수'] = df['집객시설_수'].replace(0, df['집객시설_수'].mean())
 
 df['분기당_매출_금액/점포수'] = df['분기당_매출_금액'] / df['점포수']
 df['분기당_매출_금액/점포수'] = df['분기당_매출_금액/점포수'].fillna(0)
 df['분기당_매출_금액/점포수'] = df['분기당_매출_금액/점포수'].replace(np.inf, 0)
+df['분기당_매출_건수'] = df['분기당_매출_건수'].replace(0, df['분기당_매출_건수'].mean())
 
 
 df = df.astype({'분기당_매출_금액':'int64', '분기당_매출_건수':'int64', '점포수':'int64', '유사_업종_점포_수':'int64', '개업_점포_수':'int64', '폐업_점포_수':'int64', '프랜차이즈_점포_수':'int64', '총_생활인구_수':'int64',  '남성_생활인구_수':'int64',  '여성_생활인구_수':'int64', '월_평균_소득_금액':'int64', '소득_구간_코드':'int64', '지출_총금액':'int64', '총 상주인구 수':'int64', '총_직장_인구_수':'int64', '남성_직장_인구_수':'int64', '여성_직장_인구_수':'int64', '아파트_평균_시가':'int64', '집객시설_수':'int64', '분기당_매출_금액/점포수':'int64'})
-df.to_excel("real_final_2018.xlsx")
+df.drop('분기당_매출_금액', axis=1, inplace=True)
+
+df.to_excel("real_final_2021.xlsx", index=False)
 
