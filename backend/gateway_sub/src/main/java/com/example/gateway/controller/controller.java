2bdadbd76b2815b4b9580ec1d955b2604659ad7c
손준혁 <sonjuhy@gmail.com>
2022-11-07 21:42:58 +0900

    ✨ ADD Gateway-sub Application spring cloud project
    - ADD Gateway-sub Application spring cloud project

---
+++ backend/gateway_sub/src/main/java/com/example/gateway/controller/controller.java
@@ -0,0 +1,24 @@
+package com.example.gateway.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+public class controller {
+    @Autowired
+    Environment env;
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request){
+
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("First Service Port %s", env.getProperty("local.server.port"));
+    }
+}
---
+++ backend/gateway_sub/src/main/java/com/example/gateway/filter/CustomFilter.java
@@ -0,0 +1,66 @@
+package com.example.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Objects;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom Pre Filter
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest(); // pre filter
+//            ServerHttpResponse response = exchange.getResponse(); // post filter
+
+            // Request Header 에 token 이 존재하지 않을 때
+            if(!request.getHeaders().containsKey("token")){
+                return handleUnAuthorized(exchange); // 401 Error
+            }
+
+            // Request Header 에서 token 문자열 받아오기
+            List<String> token = request.getHeaders().get("token");
+            String tokenString = Objects.requireNonNull(token).get(0);
+
+            // 토큰 검증
+            if(!tokenString.equals("A.B.C")) {
+                return handleUnAuthorized(exchange); // 토큰이 일치하지 않을 때
+            }
+
+
+            log.info("Cutsom Pre filter : request id => {}", request.getId());
+            log.info("Cutsom Pre filter : request uri => {}", request.getURI());
+
+            // Custom Post Filter
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                log.info("Custom Post filter : response code -> {}", response.getStatusCode());
+//            }));
+            return chain.filter(exchange);
+        });
+    }
+
+    private Mono<Void> handleUnAuthorized(ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+
+    public static class Config {
+        // Put the configuration properties
+    }
+}
