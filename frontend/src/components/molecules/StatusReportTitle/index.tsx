c2e10daa7fba366b29f748b60f7b9b0525197915
JangJiseon <jsmeaningood@gmail.com>
2022-11-07 11:04:02 +0900

    ✨ status 유동인구 템플릿
    - ✨ status 유동인구 템플릿

--- frontend/src/components/molecules/StatusReportTitle/index.tsx
+++ frontend/src/components/molecules/StatusReportTitle/index.tsx
@@ -10,8 +10,15 @@ interface StatusReportTitleProps {
 const StatusReportTitle = ({ title }: StatusReportTitleProps) => {
   return (
     <Wrapper>
-      <ReportTitle title={title} />
-      <ReportSummary>안녕하세요</ReportSummary>
+      <ReportTitle
+        title={title}
+        style={{ marginTop: '1rem', marginLeft: '1rem' }}
+      />
+      <ReportSummary style={{ margin: '2rem 8rem' }}>
+        <div>총 인구는 몇명입니다.</div>
+        <div>유동인구는 3명입니다.</div>
+        <div>2022년 12월 31일 점포 수는 3,229개입니다.</div>
+      </ReportSummary>
     </Wrapper>
   );
 };
