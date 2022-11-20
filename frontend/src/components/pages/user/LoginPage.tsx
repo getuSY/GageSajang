c4439fdcb2e42fc318f7de87ed44652bd75034d5
홍성목 <swff07183@gmail.com>
2022-11-21 08:49:02 +0900

    🐛 버그 수정

--- frontend/src/components/pages/user/LoginPage.tsx
+++ frontend/src/components/pages/user/LoginPage.tsx
@@ -5,10 +5,6 @@ import LoginBox from '../../organisms/LoginBox';
 const LoginPage = () => {
   return (
     <Wrapper>
-      <dialog open>
-        asd
-        <button>OK</button>
-      </dialog>
       <img
         src="/assets/img/login_img.png"
         style={loginImg}
